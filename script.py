05b174e87797e7e8bd2eb8f49fa5eb82a1135a3f
Nic Doorlay <n.doorlay@gmail.com>
2019-02-07 20:37:33 -0800

    Create script.py

---
+++ script.py
@@ -0,0 +1,13 @@
+# Main file for the Drake's equation calculator
+
+def askuser():
+  rstar = input("What is the rate of formation of stars in the galaxy?")
+  fsubp = input("What fraction of those stars have planetary systems?")
+  nsube = input("What is the number of planets per solar system that have an enviornment suitable for life?")
+  fsubl = input("What fraction of suitable planets does life occur on?")
+  fsubi = input("What fraction of life-bearing planets does intelligent life occur on?")
+  fsubc = input("What fraction of civilizations develop a technology that releases detectable signs of their existence into space?")
+  l = input("What length of time (in years) do these civilizations release detectable signs?")
+  myList = [rstar, fsubp, nsube, fsubl, fsubi, fsubc, l]
+  print(myList[1])
+  
